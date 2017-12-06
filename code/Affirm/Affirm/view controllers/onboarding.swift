a67b0fd2ff915ce0b4c07c135724cdfed2515c06
thecodingone <amanda.southworth@myds.us>
2017-12-05 19:56:38 -0800

    added podfile and view controller

---
+++ code/Affirm/Affirm/view controllers/onboarding.swift
@@ -0,0 +1,19 @@
+//
+//  onboarding.swift
+//  Affirm
+//
+//  Created by Amanda Southworth on 12/5/17.
+//  Copyright © 2017 Amanda Southworth. All rights reserved.
+//
+
+import UIKit
+
+class onboarding: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
